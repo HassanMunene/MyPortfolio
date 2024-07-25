d85aa80d90981501aae23c1047980ded34a98a61
HassanMunene <awanzihassan@gmail.com>
2024-07-25 11:32:27 +0300

    completed the skills section

---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+
+const SkillsSection = () => {
+  const skills = [
+    { name: "React" },
+    { name: "Next js",},
+    { name: "Javascript",},
+    { name: "GIT",},
+    { name: "GitHub",},
+    { name: "Html5",},
+    { name: "CSS",},
+    { name: "C++",},
+    { name: "Tailwind",},
+    { name: "Docker",},
+    { name: "Bootstrap",},
+  ];
+
+  return (
+    <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden mx-10 mb-10">
+      <InfiniteMovingCards items={skills} direction="left" speed="slow" />
+    </div>
+  );
+}
+
+export default SkillsSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import react from 'react';
 import HeaderSection from "../components/HeaderSection";
 import HeroSection from "../components/HeroSection";
+import SkillsSection from "../components/SkillsSection";
 
 const Home = () => {
   return (
     <>
       <HeaderSection />
       <HeroSection />
+      <SkillsSection />
     </>
   );
 }
